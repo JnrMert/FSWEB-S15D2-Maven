1f2471af4f18aa8f51462bddbeb85e938a4ced13
Mert Yazıcı <yzc.mert@icloud.com>
2024-10-12 18:17:24 +0300

    Failed Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.entity.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task task1 = new Task("Java Collections", "Write List Interface",
+                "Ann", Status.IN_QUEUE, Priority.LOW);
+        Task task2 = new Task("Java Collections", "Write Set Interface",
+                "Ann", Status.ASSIGNED, Priority.MED);
+        Task task3 = new Task("Java Collections", "Write Map Interface",
+                "Bob", Status.IN_QUEUE, Priority.HIGH);
+
+        Set<Task> taskSet1 = new HashSet<>();
+        taskSet1.add(task1);
+        Set<Task> taskSet2 = new HashSet<>();
+        taskSet1.add(task2);
+        Set<Task> taskSet3 = new HashSet<>();
+        taskSet1.add(task3);
+
+        TaskData taskData = new TaskData(taskSet1, taskSet2, taskSet3, new HashSet<>());
+
+        System.out.println(taskData.getTasks("all"));
     }
 }
